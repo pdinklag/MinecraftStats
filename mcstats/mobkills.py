53a7ef96b1d29d4110e8bc24006e11642148707a
Patrick Dinklage <patrick-dinklage@tu-dortmund.de>
2018-03-02 12:16:39 +0100

    Zombie and Creeper mob kill stat prototypes

--- mcstats/mobkills.py
+++ mcstats/mobkills.py
@@ -1,3 +1,31 @@
 from mcstats import __mcstats__
 
-#def createMobKillStat(name, title,)
+def createMobKillStat(name, title, mobId, mobName, iconPos):
+    __mcstats__.registry.append(
+        __mcstats__.MinecraftStat(
+            name,
+            {
+                'title': title,
+                'desc': mobName + ' killed',
+                'icon': 'minecraft-wiki/EntityCSS.png',
+                'iconPos': iconPos,
+                'unit': 'int',
+            },
+            __mcstats__.StatReader(['minecraft:killed',mobId])
+        ))
+
+# Creepers
+createMobKillStat(
+    'kill_creeper',
+    'Creeper Creep',
+    'minecraft:creeper',
+    'Creepers',
+    [16,16])
+
+# Zombies
+createMobKillStat(
+    'kill_zombie',
+    'Zombie Grinder',
+    'minecraft:zombie',
+    'Zombies',
+    [128,0])
