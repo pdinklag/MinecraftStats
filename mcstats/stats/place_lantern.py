589608bdaa34b9fc7c9da927f8e1b7e6cfab21ea
Patrick Dinklage <pdinklag@gmail.com>
2018-12-03 20:57:52 +0000

    Add Fear of the Dark award

---
+++ mcstats/stats/place_lantern.py
@@ -0,0 +1,15 @@
+from mcstats import mcstats
+
+mcstats.registry.append(
+    mcstats.MinecraftStat(
+        'place_lantern',
+        {
+            'title': 'Fear Of The Dark',
+            'desc': 'Lanterns placed',
+            'unit': 'int',
+        },
+        mcstats.StatDiffReader(
+            mcstats.StatReader(['minecraft:used','minecraft:lantern']),
+            mcstats.StatReader(['minecraft:mined','minecraft:lantern']),
+        )
+    ))
