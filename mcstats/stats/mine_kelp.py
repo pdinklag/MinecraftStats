ced8b337ee147dc65aa0cd2d9a0b2799a6df7779
Patrick Dinklage <pdinklag@gmail.com>
2018-12-03 21:05:23 +0000

    Added I Need Kelp award

---
+++ mcstats/stats/mine_kelp.py
@@ -0,0 +1,15 @@
+from mcstats import mcstats
+
+mcstats.registry.append(
+    mcstats.MinecraftStat(
+        'mine_kelp',
+        {
+            'title': 'I Need Kelp!',
+            'desc': 'Kelp mined',
+            'unit': 'int',
+        },
+        mcstats.StatSumReader([
+            mcstats.StatReader(['minecraft:mined','minecraft:kelp']),
+            mcstats.StatReader(['minecraft:mined','minecraft:kelp_plant']),
+        ])
+    ))
