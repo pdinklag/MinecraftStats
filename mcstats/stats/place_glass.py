6a077c49f71f74f3e8288d24488e09ad5ca82d26
Patrick Dinklage <pdinklag@gmail.com>
2018-12-03 21:00:36 +0000

    Update Glassworker award #17

--- mcstats/stats/place_glass.py
+++ mcstats/stats/place_glass.py
@@ -8,12 +8,7 @@ mcstats.registry.append(
             'desc': 'Glass placed',
             'unit': 'int',
         },
-        # subtract mined from placed
-        mcstats.StatDiffReader(
-            mcstats.StatSumMatchReader(
-                ['minecraft:used'],
-                ['minecraft:glass','minecraft:.*glass_pane']),
-            mcstats.StatSumMatchReader(
-                ['minecraft:mined'],
-                ['minecraft:glass','minecraft:.*glass_pane']))
+        mcstats.StatSumMatchReader(
+            ['minecraft:used'],
+            ['minecraft:glass','minecraft:.*glass_pane']),
     ))
