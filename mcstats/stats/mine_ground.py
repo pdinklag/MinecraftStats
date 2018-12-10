22bb38ef4f2a9622e4ea64345cdfa54a816ea960
Patrick Dinklage <pdinklag@gmail.com>
2018-12-10 08:15:37 +0000

    Update Excavator award

--- mcstats/stats/mine_ground.py
+++ mcstats/stats/mine_ground.py
@@ -5,12 +5,19 @@ mcstats.registry.append(
         'mine_ground',
         {
             'title': 'Excavator',
-            'desc': 'Dirt, sand and gravel mined',
+            'desc': 'Dirt, sand etc. mined',
             'unit': 'int',
         },
         mcstats.StatSumReader([
             mcstats.StatReader(['minecraft:mined','minecraft:dirt']),
+            mcstats.StatReader(['minecraft:mined','minecraft:coarse_dirt']),
+            mcstats.StatReader(['minecraft:mined','minecraft:farmland']),
+            mcstats.StatReader(['minecraft:mined','minecraft:grass']),
+            mcstats.StatReader(['minecraft:mined','minecraft:grass_path']),
             mcstats.StatReader(['minecraft:mined','minecraft:gravel']),
+            mcstats.StatReader(['minecraft:mined','minecraft:mycelium']),
+            mcstats.StatReader(['minecraft:mined','minecraft:podzol']),
+            mcstats.StatReader(['minecraft:mined','minecraft:red_sand']),
             mcstats.StatReader(['minecraft:mined','minecraft:sand']),
         ])
     ))
