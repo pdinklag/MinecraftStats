92da14f05ef33be78cb0a48e00e18cfb9bc174a0
Patrick Dinklage <pdinklag@gmail.com>
2018-12-28 10:03:29 +0000

    Add legacy support for README.TXT award. Fixes #31

--- mcstats/stats/place_sign.py
+++ mcstats/stats/place_sign.py
@@ -1,18 +1,29 @@
 from mcstats import mcstats
 
-mcstats.registry.append(
-    mcstats.MinecraftStat(
-        'place_sign',
-        {
-            'title': 'README.TXT',
-            'desc': 'Signs placed',
-            'unit': 'int',
-        },
-        # subtract mined from used
-        mcstats.StatDiffReader(
-            mcstats.StatSumMatchReader(
-                ['minecraft:used'],['minecraft:.+_sign']),
-            mcstats.StatSumMatchReader(
-                ['minecraft:mined'],['minecraft:.+_sign'])
-        )
-    ))
+mainStat = mcstats.MinecraftStat(
+    'place_sign',
+    {
+        'title': 'README.TXT',
+        'desc': 'Signs placed',
+        'unit': 'int',
+    },
+    # subtract mined from used
+    mcstats.StatDiffReader(
+        mcstats.StatSumMatchReader(
+            ['minecraft:used'],['minecraft:.+_sign']),
+        mcstats.StatSumMatchReader(
+            ['minecraft:mined'],['minecraft:.+_sign'])
+    )
+)
+
+mainStat.minVersion = 1901 # signs were updated in 18w43a
+mcstats.registry.append(mainStat)
+
+# Support for 1.13
+mcstats.registry.append(mcstats.LegacyStat(mainStat, 1451, 1631,
+    # subtract mined from used
+    mcstats.StatDiffReader(
+        mcstats.StatReader(['minecraft:used','minecraft:sign']),
+        mcstats.StatReader(['minecraft:mined','minecraft:sign'])
+    )
+))
