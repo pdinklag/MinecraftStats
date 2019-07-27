c6f6c7e14658682251363674026afaa7048ff1f6
Patrick Dinklage <pdinklag@gmail.com>
2019-07-27 08:32:20 +0000

    Added Lector award

---
+++ mcstats/stats/interact_lectern.py
@@ -0,0 +1,13 @@
+from mcstats import mcstats
+
+mcstats.registry.append(
+    mcstats.MinecraftStat(
+        'interact_lectern',
+        {
+            'title': 'Lector',
+            'desc': 'Lectern interactions',
+            'unit': 'int',
+        },
+        mcstats.StatReader(['minecraft:custom','minecraft:interact_with_stonecutter']),
+        1921 # lecterns usable since 19w02a
+    ))
