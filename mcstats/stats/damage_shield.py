7e18e8a9c57a86f9281e80ac6ed315dd23e38db0
Patrick Dinklage <pdinklag@gmail.com>
2019-07-27 08:49:41 +0000

    Add Shield award

---
+++ mcstats/stats/damage_shield.py
@@ -0,0 +1,13 @@
+from mcstats import mcstats
+
+mcstats.registry.append(
+    mcstats.MinecraftStat(
+        'damage_shield',
+        {
+            'title': 'Shield',
+            'desc': 'Damage blocked',
+            'unit': 'int',
+        },
+        mcstats.StatReader(['minecraft:custom','minecraft:damage_blocked_by_shield']),
+        1623 # stat added in 18w32a
+    ))
