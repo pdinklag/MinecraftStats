29e18f5c8f57895f33736977b3e7b9f739aaae47
Patrick Dinklage <pdinklag@gmail.com>
2019-07-27 08:42:41 +0000

    Added Smelter award

---
+++ mcstats/stats/interact_blast_furnace.py
@@ -0,0 +1,13 @@
+from mcstats import mcstats
+
+mcstats.registry.append(
+    mcstats.MinecraftStat(
+        'interact_blast_furnace',
+        {
+            'title': 'Smelter',
+            'desc': 'Blast Furnace interactions',
+            'unit': 'int',
+        },
+        mcstats.StatReader(['minecraft:custom','minecraft:interact_with_blast_furnace']),
+        1919 # blast furnace usable since 18w50a
+    ))
