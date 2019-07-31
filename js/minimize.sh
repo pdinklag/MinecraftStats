b498490703f7bb14cbaf9d255b372e0952173476
Patrick Dinklage <pdinklag@gmail.com>
2019-07-31 16:58:57 +0000

    Minimize JavaScript and CSS

---
+++ js/minimize.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+terser --source-map -o mcstats.min.js dev/mcstats.js dev/mcstats-widgets.js dev/mcstats-awardlist.js dev/mcstats-awardview.js dev/mcstats-playerlist.js dev/mcstats-playerview.js dev/mcstats-hof.js dev/mcstats-loader.js dev/mcstats-start.js
