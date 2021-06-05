f4e249279ac3bbbcb9f2288cdb8b016e316cb7a7
Patrick Dinklage <pdinklag@gmail.com>
2021-06-05 07:16:37 +0000

    Add Copper Miner award and include Copper Blocks in Compressor award. #137

--- mcstats/stats/craft_mineral_block.py
+++ mcstats/stats/craft_mineral_block.py
@@ -10,6 +10,7 @@ mcstats.registry.append(
         },
         mcstats.StatSumReader([
             mcstats.StatReader(['minecraft:crafted','minecraft:coal_block']),
+            mcstats.StatReader(['minecraft:crafted','minecraft:copper_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:diamond_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:emerald_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:gold_block']),
@@ -17,6 +18,7 @@ mcstats.registry.append(
             mcstats.StatReader(['minecraft:crafted','minecraft:lapis_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:netherite_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:quartz_block']),
+            mcstats.StatReader(['minecraft:crafted','minecraft:raw_copper_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:raw_gold_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:raw_iron_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:redstone_block']),
--- mcstats/stats/mine_ore.py
+++ mcstats/stats/mine_ore.py
@@ -20,6 +20,7 @@ def create_ore_stat(title, oreIds, oreName, minVersion = 0):
 create_ore_stat('Archeologist', ['ancient_debris'], 'Ancient Debris', 2504) # added in 20w06a
 create_ore_stat('Black Gold', ['coal_ore', 'deepslate_coal_ore'], 'Coal Ore')
 create_ore_stat('Iron Heart', ['iron_ore', 'deepslate_iron_ore'], 'Iron Ore')
+create_ore_stat('Copper Miner', ['copper_ore', 'copper_ore'], 'Copper Ore', 2681) # added in 20w45a
 create_ore_stat('Diamonds!', ['diamond_ore', 'deepslate_diamond_ore'], 'Diamond Ore')
 create_ore_stat('Gold Rush', ['gold_ore', 'deepslate_gold_ore', 'nether_gold_ore'], 'Gold Ore')
 create_ore_stat('Mountain Miner', ['emerald_ore', 'deepslate_emerald_ore'], 'Emerald Ore')
