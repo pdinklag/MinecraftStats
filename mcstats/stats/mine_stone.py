5315e0abc95b2937da1fcc579401369133bf65b2
Patrick Dinklage <pdinklag@gmail.com>
2021-06-05 06:31:22 +0000

    Include new stone blocks in Stonemason award. #137

--- mcstats/stats/mine_stone.py
+++ mcstats/stats/mine_stone.py
@@ -10,10 +10,14 @@ mcstats.registry.append(
         },
         mcstats.StatSumReader([
             mcstats.StatReader(['minecraft:mined','minecraft:stone']),
-            mcstats.StatReader(['minecraft:mined','minecraft:diorite']),
             mcstats.StatReader(['minecraft:mined','minecraft:andesite']),
-            mcstats.StatReader(['minecraft:mined','minecraft:granite']),
-            mcstats.StatReader(['minecraft:mined','minecraft:basalt']),
             mcstats.StatReader(['minecraft:mined','minecraft:blackstone']),
+            mcstats.StatReader(['minecraft:mined','minecraft:basalt']),
+            mcstats.StatReader(['minecraft:mined','minecraft:calcite']),
+            mcstats.StatReader(['minecraft:mined','minecraft:deepslate']),
+            mcstats.StatReader(['minecraft:mined','minecraft:diorite']),
+            mcstats.StatReader(['minecraft:mined','minecraft:granite']),
+            mcstats.StatReader(['minecraft:mined','minecraft:smooth_basalt']),
+            mcstats.StatReader(['minecraft:mined','minecraft:tuff']),
         ])
     ))
