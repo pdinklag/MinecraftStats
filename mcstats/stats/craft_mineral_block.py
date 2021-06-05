c7fd50c747d061fae117baabbc47fc8cb8474dbb
Patrick Dinklage <pdinklag@gmail.com>
2021-06-05 07:07:49 +0000

    Include Raw Gold and Raw Iron blocks in Compressor award. #137

--- mcstats/stats/craft_mineral_block.py
+++ mcstats/stats/craft_mineral_block.py
@@ -10,13 +10,15 @@ mcstats.registry.append(
         },
         mcstats.StatSumReader([
             mcstats.StatReader(['minecraft:crafted','minecraft:coal_block']),
-            mcstats.StatReader(['minecraft:crafted','minecraft:iron_block']),
-            mcstats.StatReader(['minecraft:crafted','minecraft:gold_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:diamond_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:emerald_block']),
+            mcstats.StatReader(['minecraft:crafted','minecraft:gold_block']),
+            mcstats.StatReader(['minecraft:crafted','minecraft:iron_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:lapis_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:netherite_block']),
-            mcstats.StatReader(['minecraft:crafted','minecraft:redstone_block']),
             mcstats.StatReader(['minecraft:crafted','minecraft:quartz_block']),
+            mcstats.StatReader(['minecraft:crafted','minecraft:raw_gold_block']),
+            mcstats.StatReader(['minecraft:crafted','minecraft:raw_iron_block']),
+            mcstats.StatReader(['minecraft:crafted','minecraft:redstone_block']),
         ])
     ))
