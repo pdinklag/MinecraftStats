49a99c57b7bcf713010030d49c3bf70848aca501
Patrick Dinklage <pdinklag@gmail.com>
2023-05-27 08:20:00 +0200

    A nicer way to tell the user that the config file does not exist

--- src/main/java/de/pdinklag/mcstats/cli/MinecraftStatsCLI.java
+++ src/main/java/de/pdinklag/mcstats/cli/MinecraftStatsCLI.java
@@ -1,6 +1,7 @@
 package de.pdinklag.mcstats.cli;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,28 +13,35 @@ public class MinecraftStatsCLI {
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     private static String getJarFilename() {
-        final File jarFile = new File(MinecraftStatsCLI.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        final File jarFile = new File(
+                MinecraftStatsCLI.class.getProtectionDomain().getCodeSource().getLocation().getPath());
         return jarFile.getName();
     }
 
     /**
      * The main method.
+     * 
      * @param args the command-line arguments passed to the application
      */
     public static void main(String[] args) {
-        if(args.length > 0) {
+        if (args.length > 0) {
             final StdoutLogWriter log = new StdoutLogWriter();
             try {
                 // load config
                 final Path configPath = Path.of(args[0]);
-                JSONConfig config = new JSONConfig(configPath);
+                if (Files.isRegularFile(configPath)) {
+                    JSONConfig config = new JSONConfig(configPath);
 
-                // run updater
-                CLIUpdater updater = new CLIUpdater(config, log);
-                updater.run();
+                    // run updater
+                    CLIUpdater updater = new CLIUpdater(config, log);
+                    updater.run();
 
-                log.writeLine("[" + LocalDateTime.now().format(DATE_FORMAT) + "] update finished");
-            } catch(Exception e) {
+                    log.writeLine("[" + LocalDateTime.now().format(DATE_FORMAT) + "] update finished");
+                } else {
+                    System.err.println("Configuration file not found: " + configPath);
+                    System.exit(1);
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         } else {
